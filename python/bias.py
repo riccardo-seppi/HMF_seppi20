ba2124090ae0ee35cab3ece11d2d2071be4729f9
Riccardo Seppi <rseppi@ds54.mpe.mpg.de>
2021-03-02 15:53:52 +0100

    fixing scales in example of bias

--- python/bias.py
+++ python/bias.py
@@ -19,23 +19,28 @@ h=cosmo.Hz(z=0)/100
 dc0 = peaks.collapseOverdensity(z = 0)
 
 
-M = np.logspace(12,16,100)
-z = np.array([0.0,0.2,0.5,1.0,2.0,5.0])
+M = np.logspace(12,15,100)
+z = np.array([0.0,0.1,0.2,0.4,0.6,0.8,1.0])
 
+Mdefs = ['200c','500c']
+for Mdef in Mdefs:
+	print(Mdef)
+	outfig = os.path.join('/home/rseppi/HMF_seppi20','figures','bias_'+Mdef+'.png')
+	plt.figure(figsize=(6,6))
 
-outfig = os.path.join('/home/rseppi/HMF_seppi20','figures','bias.png')
-plt.figure(figsize=(6,6))
+	for z_ in z:
+    		b = bias.haloBias(M, model = 'tinker10', z = z_, mdef = Mdef)
+    		plt.plot(M,b, label='z={:.1f}'.format(z_))
 
-for z_ in z:
-    b = bias.haloBias(M, model = 'tinker10', z = z_, mdef = '500c')
-    plt.plot(M,b, label='z={:.1f}'.format(z_))
-
-plt.xlabel(r'M500c [M$_\odot$/h]', fontsize=13)
-plt.ylabel('b', fontsize=13)
-plt.xscale('log')
-plt.tick_params(labelsize=13)
-plt.grid(True)
-plt.savefig(outfig)    
+	plt.xlabel(r'M'+Mdef+' [M$_\odot$/h]', fontsize=13)
+	plt.ylabel('b', fontsize=13)
+	plt.xscale('log')
+	plt.ylim(0,6)
+	plt.legend(fontsize=13)
+	plt.tick_params(labelsize=13)
+	plt.grid(True)
+	plt.tight_layout()
+	plt.savefig(outfig)    
 
 
 
